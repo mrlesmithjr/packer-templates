86f4d95fbdd1bb991f26de075349272d5795a70a
Larry Smith Jr <mrlesmithjr@gmail.com>
2015-09-09 21:01:10 -0400

    trying to fix CentOS template

--- CentOS/6.5/scripts/base.sh
+++ CentOS/6.5/scripts/base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl wget
+yum -y install gcc make gcc-c++ kernel-devel.x86_64 perl wget
--- CentOS/6.5/scripts/virtualbox.sh
+++ CentOS/6.5/scripts/virtualbox.sh
@@ -2,7 +2,12 @@
 
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
+#yum -y groupinstall 'Development Tools'
+#wget http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
+#rpm -Uvh rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
+#yum -y --enablerepo=rpmforge install dkms
 mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
+/etc/rc.d/init.d/vboxadd setup
 rm -rf /home/vagrant/VBoxGuestAdditions_*.iso

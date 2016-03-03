db736c534dc46f71ac084dabcfd6bdd973c214ae
Larry Smith Jr <mrlesmithjr@gmail.coml>
2016-03-03 15:06:07 -0500

    Updated templates and scripts
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.coml>

--- CentOS/7/scripts/base.sh
+++ CentOS/7/scripts/base.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-yum -y install gcc make gcc-c++ kernel-devel.x86_64 perl wget
+yum -y install epel-release
+yum -y install facter gcc make gcc-c++ kernel-devel.x86_64 perl wget

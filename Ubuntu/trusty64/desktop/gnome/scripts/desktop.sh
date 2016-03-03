8d18c0bdb42cf3dce66beea9467a8d919328a967
Larry Smith Jr <mrlesmithjr@gmail.coml>
2016-03-03 01:13:05 -0500

    Cleaned up files and templates
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.coml>

---
+++ Ubuntu/trusty64/desktop/gnome/scripts/desktop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "==> Installing gnome-desktop"
+apt-get install -y --no-install-recommends ubuntu-gnome-desktop
+apt-get install -y chromium-browser firefox
---
+++ Ubuntu/trusty64/desktop/unity/scripts/desktop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "==> Installing ubuntu-desktop"
+apt-get install -y --no-install-recommends ubuntu-desktop

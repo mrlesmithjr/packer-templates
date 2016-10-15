01449d2ca3d1fdd5a2d1cb8c00ee088b8719595a
Larry Smith Jr <mrlesmithjr@gmail.com>
2016-10-15 18:24:00 -0400

    Temp fix to ensure this script runs
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- Ubuntu/yakkety64/scripts/virtualbox.sh
+++ Ubuntu/yakkety64/scripts/virtualbox.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Bail if we are not running inside VirtualBox.
-if [[ `facter virtual` != "virtualbox" ]]; then
-    exit 0
-fi
+# if [[ `facter virtual` != "virtualbox" ]]; then
+#     exit 0
+# fi
 
 mkdir -p /mnt/virtualbox
 mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox

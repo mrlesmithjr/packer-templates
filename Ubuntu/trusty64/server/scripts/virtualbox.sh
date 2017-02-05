83b589a96bba3297de0d3a0cfd792c630f31c5b2
Larry Smith Jr <mrlesmithjr@gmail.com>
2017-02-05 16:11:19 -0500

    Fixed virtualbox guest install
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- Ubuntu/trusty64/server/scripts/virtualbox.sh
+++ Ubuntu/trusty64/server/scripts/virtualbox.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Bail if we are not running inside VirtualBox.
-if [[ `facter virtual` != "virtualbox" ]]; then
-    exit 0
-fi
-
 mkdir -p /mnt/virtualbox
 mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
 sh /mnt/virtualbox/VBoxLinuxAdditions.run

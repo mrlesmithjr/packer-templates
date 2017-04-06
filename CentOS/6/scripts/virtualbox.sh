a69ec9a4af7072427f74c41ca53b7458be431878
Larry Smith Jr <mrlesmithjr@gmail.com>
2017-04-05 21:08:53 -0400

    Updated CentOS 6 to 6.9
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- CentOS/6/scripts/virtualbox.sh
+++ CentOS/6/scripts/virtualbox.sh
@@ -2,10 +2,6 @@
 
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
-#yum -y groupinstall 'Development Tools'
-#wget http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
-#rpm -Uvh rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
-#yum -y --enablerepo=rpmforge install dkms
 mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt

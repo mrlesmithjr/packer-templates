c9c2849ad9952074200af33a39d744d590bc1e25
Larry Smith Jr <mrlesmithjr@gmail.com>
2018-04-26 21:57:07 -0400

    Added Ubuntu 18.04 Bionic Beaver

---
+++ Ubuntu/bionic64/server/scripts/virtualbox.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /mnt/virtualbox
+mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
+sh /mnt/virtualbox/VBoxLinuxAdditions.run
+ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
+umount /mnt/virtualbox
+rm -rf /home/vagrant/VBoxGuest*.iso

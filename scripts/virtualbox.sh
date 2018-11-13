7d23fa30af170c3764f3ec6847c06ec941300e60
Larry Smith Jr <mrlesmithjr@gmail.com>
2018-11-13 00:53:15 -0500

    Added missing removal of VBox ISO. Also fixed incorrect path.

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -13,12 +13,13 @@ if [ -f /etc/debian_version ]; then
     os="$(facter operatingsystem)"
     if [[ $os == "Ubuntu" ]]; then
         sudo apt-get install -y virtualbox-guest-utils
+        sudo rm -rf /home/vagrant/VBoxGuestAdditions*.iso
         elif [[ $os == "Debian" ]]; then
         sudo mkdir -p /mnt/virtualbox
-        sudo mount -o loop /home/packer/VBoxGuestAdditions.iso /mnt/virtualbox
+        sudo mount -o loop /home/vagrant/VBoxGuestAdditions*.iso /mnt/virtualbox
         sudo sh /mnt/virtualbox/VBoxLinuxAdditions.run
         sudo umount /mnt/virtualbox
-        sudo rm -rf /home/packer/VBoxGuestAdditions.iso
+        sudo rm -rf /home/vagrant/VBoxGuestAdditions*.iso
     fi
 fi
 

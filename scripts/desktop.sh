4af9fd563f84da95aae68ea0c481344091e75ca9
Larry Smith Jr <mrlesmithjr@gmail.com>
2018-11-13 09:08:40 -0500

    Added Ubuntu Xenial Desktop

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -4,17 +4,28 @@ set -x
 
 if [ -f /etc/debian_version ]; then
     os="$(facter operatingsystem)"
+    os_release="$(facter operatingsystemrelease)"
     if [[ $os == "Ubuntu" ]]; then
         echo "==> Installing ubuntu-desktop"
-        apt-get install -y --no-install-recommends ubuntu-desktop
+        sudo apt-get install -y --no-install-recommends ubuntu-desktop
         
         USERNAME=vagrant
-        GDM_CUSTOM_CONFIG=/etc/gdm3/custom.conf
+        if [[ $os_release < 18.04  ]]; then
+            GDM_CUSTOM_CONFIG=/etc/gdm/custom.conf
+            LIGHTDM_CONFIG=/etc/lightdm/lightdm.conf
+            
+            echo "==> Configuring lightdm autologin"
+            sudo bash -c "echo "[SeatDefaults]" >> $LIGHTDM_CONFIG"
+            sudo bash -c "echo "autologin-user=${USERNAME}" >> $LIGHTDM_CONFIG"
+        else
+            GDM_CUSTOM_CONFIG=/etc/gdm3/custom.conf
+        fi
         
-        mkdir -p "$(dirname ${GDM_CUSTOM_CONFIG})"
-        echo "[daemon]" >> $GDM_CUSTOM_CONFIG
-        echo "# Enabling automatic login" >> $GDM_CUSTOM_CONFIG
-        echo "AutomaticLoginEnable=True" >> $GDM_CUSTOM_CONFIG
-        echo "AutomaticLogin=${USERNAME}" >> $GDM_CUSTOM_CONFIG
+        echo "==> Configuring lightdm autologin"
+        sudo mkdir -p "$(dirname ${GDM_CUSTOM_CONFIG})"
+        sudo bash -c "echo "[daemon]" >> $GDM_CUSTOM_CONFIG"
+        sudo bash -c "echo "# Enabling automatic login" >> $GDM_CUSTOM_CONFIG"
+        sudo bash -c "echo "AutomaticLoginEnable=True" >> $GDM_CUSTOM_CONFIG"
+        sudo bash -c "echo "AutomaticLogin=${USERNAME}" >> $GDM_CUSTOM_CONFIG"
     fi
 fi

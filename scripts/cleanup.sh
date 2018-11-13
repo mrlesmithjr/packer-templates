35b3aeac832fb8fb4ccb4a0efa1be9e36084a474
Larry Smith Jr <mrlesmithjr@gmail.com>
2018-11-13 00:33:23 -0500

    Cleaned up scripts

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -5,21 +5,21 @@ set -x
 
 # Debian/Ubuntu
 if [ -f /etc/debian_version ]; then
-  codename="$(facter lsbdistcodename)"
-  #cleanup apt
-  sudo apt-get clean
+    codename="$(facter lsbdistcodename)"
+    #cleanup apt
+    sudo apt-get clean
 fi
 
 # RHEL
 if [ -f /etc/redhat-release ]; then
-  codename="$(facter operatingsystem)"
-  if [[ $codename != "Fedora" ]]; then
-    sudo yum clean all
-    sudo rm -rf /var/cache/yum
-  fi
-  if [[ $codename == "Fedora" ]]; then
-    sudo dnf clean all
-  fi
+    codename="$(facter operatingsystem)"
+    if [[ $codename != "Fedora" ]]; then
+        sudo yum clean all
+        sudo rm -rf /var/cache/yum
+    fi
+    if [[ $codename == "Fedora" ]]; then
+        sudo dnf clean all
+    fi
 fi
 
 #Stop services for cleanup
@@ -27,18 +27,18 @@ sudo service rsyslog stop
 
 #clear audit logs
 if [ -f /var/log/audit/audit.log ]; then
-  sudo bash -c "cat /dev/null > /var/log/audit/audit.log"
+    sudo bash -c "cat /dev/null > /var/log/audit/audit.log"
 fi
 if [ -f /var/log/wtmp ]; then
-  sudo bash -c "cat /dev/null > /var/log/wtmp"
+    sudo bash -c "cat /dev/null > /var/log/wtmp"
 fi
 if [ -f /var/log/lastlog ]; then
-  sudo bash -c "cat /dev/null > /var/log/lastlog"
+    sudo bash -c "cat /dev/null > /var/log/lastlog"
 fi
 
 #cleanup persistent udev rules
 if [ -f /etc/udev/rules.d/70-persistent-net.rules ]; then
-  sudo rm /etc/udev/rules.d/70-persistent-net.rules
+    sudo rm /etc/udev/rules.d/70-persistent-net.rules
 fi
 
 #cleanup /tmp directories
--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -7,10 +7,10 @@ if [ -f /etc/debian_version ]; then
     if [[ $os == "Ubuntu" ]]; then
         echo "==> Installing ubuntu-desktop"
         apt-get install -y --no-install-recommends ubuntu-desktop
-
+        
         USERNAME=vagrant
         GDM_CUSTOM_CONFIG=/etc/gdm3/custom.conf
-
+        
         mkdir -p "$(dirname ${GDM_CUSTOM_CONFIG})"
         echo "[daemon]" >> $GDM_CUSTOM_CONFIG
         echo "# Enabling automatic login" >> $GDM_CUSTOM_CONFIG

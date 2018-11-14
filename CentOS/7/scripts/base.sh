49ca2d67a8d0a1fca303e3b03d7b1942e2dd2fb3
Larry Smith Jr <mrlesmithjr@gmail.com>
2018-11-13 22:22:51 -0500

    Updated CentOS versions to new structure

--- CentOS/7/scripts/base.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-yum -y install epel-release
-yum -y install facter gcc make gcc-c++ kernel-devel.x86_64 perl wget
--- CentOS/7/scripts/virtualbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
-cd /tmp
-mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
-sh /mnt/VBoxLinuxAdditions.run
-umount /mnt
-/etc/rc.d/init.d/vboxadd setup
-rm -rf /home/vagrant/VBoxGuestAdditions_*.iso

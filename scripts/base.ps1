c8804ce4512f888d9a6b1281917fd1391088e12d
Larry Smith Jr <mrlesmithjr@gmail.com>
2018-11-24 10:21:00 -0500

    Added Windows 2008 R2. But it not working correctly yet.

--- scripts/base.ps1
+++ scripts/base.ps1
@@ -1,10 +1,6 @@
 $ansible_script = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
 $chocolatey_script = "https://chocolatey.org/install.ps1"
 
-# Setup WinRM for Ansible Management
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex (
-    (New-Object System.Net.WebClient).DownloadString($ansible_script))
-
 # Install Chocolatey Package Manager
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex (
     (New-Object System.Net.WebClient).DownloadString($chocolatey_script))
@@ -15,3 +11,14 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex (
 
 # Install Chocolatey Packages
 choco install -y 7zip sdelete
+
+if ($PSVersionTable.PSVersion.Major -lt 3) {
+    Write-Host "Upgrading PowerShell"
+    choco upgrade -y dotnet4.5
+    choco upgrade -y powershell
+    refreshenv
+}
+
+# Setup WinRM for Ansible Management
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex (
+    (New-Object System.Net.WebClient).DownloadString($ansible_script))

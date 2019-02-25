edbec576eddbb1b23210e71c9d717393a2cd196d
Larry Smith Jr <mrlesmithjr@gmail.com>
2019-02-25 09:46:42 -0500

    Added VMware build for FreeNAS

--- FreeNAS/11.2/server/build.sh
+++ FreeNAS/11.2/server/build.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-packer build -only=virtualbox-iso -var-file=../../../private_vars.json -var-file=box_info.json -var-file=template.json ../../freenas-server.json
+packer build -var-file=../../../private_vars.json -var-file=box_info.json -var-file=template.json ../../freenas-server.json
--- scripts/freenas.sh
+++ scripts/freenas.sh
@@ -42,3 +42,7 @@ curl -X POST \
    "bsdusr_sshpubkey": "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key",
    "bsdusr_sudo": true
 }'
+
+# if [ "$PACKER_BUILDER_TYPE" == "vmware-iso" ]; then
+#     pkg install --yes open-vm-tools-nox11
+# fi

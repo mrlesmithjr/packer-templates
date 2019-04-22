e9a14834e806a2efc38069d9d67c8ebcf6ea2335
Larry Smith Jr <mrlesmithjr@gmail.com>
2019-04-22 08:17:34 -0400

    Refactored Windows builds and added windows updates

--- scripts/base.ps1
+++ scripts/base.ps1
@@ -2,8 +2,8 @@ $ansible_script = "https://raw.githubusercontent.com/ansible/ansible/devel/examp
 $chocolatey_script = "https://chocolatey.org/install.ps1"
 
 # Install Chocolatey Package Manager
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex (
-    (New-Object System.Net.WebClient).DownloadString($chocolatey_script))
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression (
+  (New-Object System.Net.WebClient).DownloadString($chocolatey_script))
 
 # Enable Remote Desktop
 (Get-WmiObject Win32_TerminalServiceSetting -Namespace root\cimv2\TerminalServices).SetAllowTsConnections(1, 1)
@@ -12,13 +12,6 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex (
 # Install Chocolatey Packages
 choco install -y 7zip sdelete
 
-if ($PSVersionTable.PSVersion.Major -lt 3) {
-    Write-Host "Upgrading PowerShell"
-    choco upgrade -y dotnet4.5
-    choco upgrade -y powershell
-    refreshenv
-}
-
 # Setup WinRM for Ansible Management
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex (
-    (New-Object System.Net.WebClient).DownloadString($ansible_script))
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression (
+  (New-Object System.Net.WebClient).DownloadString($ansible_script))

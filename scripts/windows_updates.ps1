615baeab7ed9880b237687b4ff77c82f426575b8
Larry Smith Jr <mrlesmithjr@gmail.com>
2019-04-23 17:40:50 -0400

    Additional Windows adjustments

--- scripts/windows_updates.ps1
+++ scripts/windows_updates.ps1
@@ -31,11 +31,11 @@ function Check-ContinueRestartOrEnd() {
       }
       elseif ($script:Cycles -gt $global:MaxCycles) {
         LogWrite "Exceeded Cycle Count - Stopping"
-        Invoke-Expression "a:\bootstrap.ps1 -user Administrator -password vagrant -Verbose"
+        Invoke-Expression "a:\bootstrap.ps1 -user vagrant -password vagrant -Verbose"
       }
       else {
         LogWrite "Done Installing Windows Updates"
-        Invoke-Expression "a:\bootstrap.ps1 -user Administrator -password vagrant -Verbose"
+        Invoke-Expression "a:\bootstrap.ps1 -user vagrant -password vagrant -Verbose"
       }
     }
     1 {
@@ -132,7 +132,7 @@ function Install-WindowsUpdates() {
     LogWrite 'No updates available to install...'
     $global:MoreUpdates = 0
     $global:RestartRequired = 0
-    Invoke-Expression "a:\bootstrap.ps1 -user Administrator -password vagrant -Verbose"
+    Invoke-Expression "a:\bootstrap.ps1 -user vagrant -password vagrant -Verbose"
     break
   }
 

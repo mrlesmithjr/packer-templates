649402a089e794b6a4c9a07e5255b33ac6371e5c
Larry Smith Jr <mrlesmithjr@gmail.com>
2019-05-13 08:12:05 -0400

    Added additional info about what is going on during script execution

--- scripts/virtualbox.ps1
+++ scripts/virtualbox.ps1
@@ -1,15 +1,19 @@
 if ($env:PACKER_BUILDER_TYPE -eq "virtualbox-iso") {
   Write-Host "Installing Guest Additions"
   if (Test-Path d:\VBoxWindowsAdditions.exe) {
+    Write-Host "Mounting Drive with VBoxWindowsAdditions"
+    Write-Host "Adding Virtualbox trusted certs."
     Start-Process -FilePath "d:\cert\VBoxCertUtil.exe" -ArgumentList "add-trusted-publisher d:\cert\vbox-sha1.cer" -Wait -NoNewWindow
     Start-Process -FilePath "d:\cert\VBoxCertUtil.exe" -ArgumentList "add-trusted-publisher d:\cert\vbox-sha256.cer" -Wait -NoNewWindow
-    Write-Host "Mounting Drive with VBoxWindowsAdditions"
+    Write-Host "Installing Virtualbox guest additions."
     Start-Process -FilePath "d:\VBoxWindowsAdditions.exe" -ArgumentList "/S" -Wait
   }
   if (Test-Path e:\VBoxWindowsAdditions.exe) {
+    Write-Host "Mounting Drive with VBoxWindowsAdditions"
+    Write-Host "Adding Virtualbox trusted certs."
     Start-Process -FilePath "e:\cert\VBoxCertUtil.exe" -ArgumentList "add-trusted-publisher e:\cert\vbox-sha1.cer" -Wait -NoNewWindow
     Start-Process -FilePath "e:\cert\VBoxCertUtil.exe" -ArgumentList "add-trusted-publisher e:\cert\vbox-sha256.cer" -Wait -NoNewWindow
-    Write-Host "Mounting Drive with VBoxWindowsAdditions"
+    Write-Host "Installing Virtualbox guest additions."
     Start-Process -FilePath "e:\VBoxWindowsAdditions.exe" -ArgumentList "/S" -Wait
   }
 }

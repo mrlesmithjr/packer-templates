33d6aad8d933990517f44749bd7bfb4c30d16fd7
Larry Smith Jr <mrlesmithjr@gmail.com>
2020-08-06 22:26:07 -0400

    Generated new build for Ubuntu 16.04 and added QEMU back

--- Ubuntu/xenial64/server/build.sh
+++ Ubuntu/xenial64/server/build.sh
@@ -2,15 +2,15 @@
 packer build -except=qemu -var-file=../../../private_vars.json \
   -var-file=box_info.json -var-file=template.json ../../ubuntu-server.json
 
-# command -v qemu-system-x86_64 --version >/dev/null 2>&1
-# QEMU_CHECK=$?
-# if [ $QEMU_CHECK -eq 0 ]; then
-#     if [[ $(uname) == "Darwin" ]]; then
-#       QEMU_ACCEL="hvf"
-#     elif [[ $(uname) == "Linux" ]]; then
-#       QEMU_ACCEL="kvm"
-#     fi
-#     packer build -only=qemu -var qemu_accelerator=$QEMU_ACCEL \
-#       -var-file=../../../private_vars.json -var-file=box_info.json \
-#       -var-file=template.json ../../ubuntu-server.json
-# fi
+command -v qemu-system-x86_64 --version >/dev/null 2>&1
+QEMU_CHECK=$?
+if [ $QEMU_CHECK -eq 0 ]; then
+  if [[ $(uname) == "Darwin" ]]; then
+    QEMU_ACCEL="hvf"
+  elif [[ $(uname) == "Linux" ]]; then
+    QEMU_ACCEL="kvm"
+  fi
+  packer build -only=qemu -var qemu_accelerator=$QEMU_ACCEL \
+    -var-file=../../../private_vars.json -var-file=box_info.json \
+    -var-file=template.json ../../ubuntu-server.json
+fi
